9037b0aea781591199a68f2b4766ce2e9eecb091
pheaktra1402 <pheaktrathorn474@email.com>
2026-07-20 20:42:01 +0700

    prepare

--- database/migrations/2026_07_20_040744_create_categories_table.php
+++ database/migrations/2026_07_20_040744_create_categories_table.php
@@ -9,17 +9,32 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    // public function up(): void
+    // {
+    //     Schema::create('categories', function (Blueprint $table) {
+    //         $table->id();
+    //         $table->string('category_code', 50)->unique();
+    //         $table->string('category_name', 150);
+    //         $table->text('description')->nullable();
+    //         $table->enum('status', ['Active', 'Inactive'])->default('Active');
+    //         $table->timestamps();
+    //     });
+    // }
+
+public function up(): void
+{
+    // Check if the table DOES NOT exist before trying to create it
+    if (!Schema::hasTable('categories')) {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('category_code', 50)->unique();
+            $table->string('category_code', 50);
             $table->string('category_name', 150);
             $table->text('description')->nullable();
             $table->enum('status', ['Active', 'Inactive'])->default('Active');
             $table->timestamps();
         });
     }
+}
 
     /**
      * Reverse the migrations.
